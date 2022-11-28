58549e6ee04d35c12ab432dbebe9dbf96c1197ba
YOURFRIEND259 <kubabor1@gmail.com>
2022-11-28 17:57:17 +0100

    PAUSE WORKS AGAIN

--- mp3player.py
+++ mp3player.py
@@ -5,12 +5,7 @@ from pygame import mixer
 
 root=tk.Tk()
 songbox=tk.Listbox(root,bg="black",fg="lightgreen",width=60)
-
 songz={}
-
-# songz={1:2,6:9}
-# for key in songz:
-#     songbox.insert(tk.END, '{}: {}'.format(key, songz[key]))
 
 class Musicplayer:
     def __init__(self,window) -> None:
@@ -53,73 +48,84 @@ class Musicplayer:
         STATEMENTS
         """
         self.playing=False
-        # self.music_file=False
         self.ispaused=False
-    global song
-    song= songbox.get(tk.ACTIVE)
+        self.SONG_END = pg.USEREVENT + 1
+        self.actual_song = 0
+        self.playlist=list()
 
+    def load(self):
+        directory=filedialog.askopenfilenames()
+        for i in directory:
+            self.playlist.append(i)
+        for i in directory:
+            song_name=i[i.rfind('/')+1:]
+            songz[i]=song_name
+            print(songz)
+            try:
+                songbox.insert(tk.END,song_name)
+                self.play_restart.set("Play")
+            except:
+                print("error")
 
     """
     BUTTON FUNCTIONS
     """
+    # global ispaused
+    # ispaused=False
+
     def play(self):
-        
-        song= songbox.get(tk.ACTIVE)
-        mixer.init()
-        mixer.music.load((songz[song]))
-        if not self.playing:   
+        if self.playlist:
+            directory=self.playlist[self.actual_song ]
+            mixer.init()
+            mixer.music.load(directory)
+            mixer.music.set_volume(0.01)
+            mixer.music.set_endevent(self.SONG_END)
             mixer.music.play(1, 0.0)
-            self.playing=True
-            self.ispaused=False
-            self.play_restart.set('Pause')
-            print("gra")
-
-        elif not self.ispaused and self.playing:
-            mixer.music.pause()
-            self.ispaused=True
-            self.play_restart.set('Play')
-            print("pause",self.ispaused)
-        elif self.ispaused and self.playing:
-            mixer.music.unpause()
-            self.ispaused=False
-            self.play_restart.set('Pause')
-            print("resume",self.ispaused)
-        # self.playing_state = False
-        # self.play_restart.set('Restart')
-        # self.pause_resume.set('Pause')
-
-    def load(self):
-        self.music_file=filedialog.askopenfilenames(filetypes=(("mp3 Files","*mp3"),("wav Files","*wav")))
-        
-        for i in self.music_file:
-            songsource=i
-            i = i[i.rfind('/')+1:]
-            songz[i]=songsource
-            try:
-                songbox.insert(tk.END,i)
-                self.play_restart.set("Play")
+            
+            if not self.playing:   
                 
-                
-            except:
-                print("error")
+                self.playing=True
+                # self.ispaused=False
+                self.play_restart.set('Pause')
+                print("gra")
+
+            elif not self.ispaused and self.playing:
+                mixer.music.pause()
+                print("pauza")
+                self.ispaused=True
+                self.play_restart.set('Resume')
+                print("pause",self.ispaused)
+
+            elif self.ispaused and self.playing:
+                mixer.music.unpause()
+                print("po pauzie?????????")
+                self.ispaused=False
+                self.play_restart.set('Pause')
+                print("resume",self.ispaused)
+            
+            # self.playing_state = False
+            # self.play_restart.set('Restart')
+            # self.pause_resume.set('Pause')
+        else:
+            print("empty playlist")
     
 
     def forward(self):
-        # global select
-        # select=songbox.curselection()
-        # if select[0]>=len(songz)-1:
-        #     next_song=(list(songz.keys())[0])
-        #     print(next_song)
-        #     select=select[0]+1
-        # else:
-        #     next_song=(list(songz.keys())[select[0]+1])
-        #     print(next_song)
-        #     select=select[0]+1
-        next_song=songbox.curselection()
-        if next_song[0]>=len(songz)-1:
-            next_song=next_song[0]+1
+        global select
+        select=songbox.curselection()
+        if select[0]>=len(songz)-1:
+            next_song=(list(songz.keys())[0])
+            print(next_song)
+            select=select[0]+1
         else:
-            next_song=next_song[0]-len(songz)-1
+            next_song=(list(songz.keys())[select[0]+1])
+            print(next_song)
+            select=select[0]+1
+        # next_song=songbox.curselection()
+        # if next_song[0]>=len(songz)-1:
+        #     next_song=next_song[0]+1
+        # else:
+        #     next_song=next_song[0]-len(songz)-1
         song=songbox.get(next_song)
         mixer.music.load(songz[song])
         print("select",songz[song])
@@ -148,6 +154,7 @@ class Musicplayer:
     def stop(self):
         mixer.music.stop()
         self.playing=False
+        print("stop")
         self.play_restart.set('Play')
         
     # def pause(self):
@@ -164,6 +171,7 @@ class Musicplayer:
         # if (self.isrepeat==1):
         #     mixer.music.play(-1)
         pass
+    
     
 """
 PROGRAM'S LOOP
